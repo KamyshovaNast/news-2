689256c10ad1f8e6542cf0999925f1318d78ca79
Kamyshova Anastasia <kamyshova.nast@gmail.com>
2021-01-21 20:20:01 +0300

    Axios added (full)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
 import router from './router'
 import vuetify from './plugins/vuetify';
 
