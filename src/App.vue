d48565fc987d8fa8f036d20b667799084da9c5d4
Kamyshova Anastasia <kamyshova.nast@gmail.com>
2021-01-27 17:12:33 +0300

    Without router

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,103 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
     <v-app-bar
       app
-      color="primary"
-      dark
+      shrink-on-scroll
+    class="light-green lighten-4"
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      
+      <v-spacer></v-spacer>
+      <v-btn elevation="2" class="new" v-on:click="getNewsRu">Новости</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn elevation="2" class="new" v-on:click="getNewsGb">News (GB)</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn elevation="2" class="new" v-on:click="getNewsUs">News (USA)</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn elevation="2" class="new" v-on:click="getNewsGe">Die Nachrichten</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn elevation="2" class="new" v-on:click="getNewsFr">Nouvelles</v-btn>
+      
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
     </v-app-bar>
 
-    <v-main>
-      <HelloWorld/>
+    <v-main class="light-green lighten-5">
+      <v-container>
+        <v-row>
+          <v-col v-for="i in news"
+            :key="i"
+            cols="4">
+            <v-card>
+                <v-card-title>{{i.title}}</v-card-title>
+                <v-card-text>{{i.description}}</v-card-text>
+                <img v-bind:src="i.urlToImage">
+                <v-card-actions><a v-bind:href="i.url">Источник</a></v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+    export default {
+        name: 'App',
+        data() {
+            return {
+                news: {}
+            }
+        },
+        methods: {
+            getNewsRu() {
+                this.axios
+                .get("https://newsapi.org/v2/top-headlines?country=ru&apiKey=2a9013172f7441c5b6f2e7a0d539660a")
+                .then((response) => {this.news = response.data.articles})
+            },
+            getNewsUs() {
+                this.axios
+                .get("https://newsapi.org/v2/top-headlines?country=us&apiKey=2a9013172f7441c5b6f2e7a0d539660a")
+                .then((response) => {this.news = response.data.articles})
+            },
+            getNewsGb() {
+                this.axios
+                .get("https://newsapi.org/v2/top-headlines?country=gb&apiKey=2a9013172f7441c5b6f2e7a0d539660a")
+                .then((response) => {this.news = response.data.articles})
+            },
+            getNewsGe() {
+                this.axios
+                .get("https://newsapi.org/v2/top-headlines?country=de&apiKey=2a9013172f7441c5b6f2e7a0d539660a")
+                .then((response) => {this.news = response.data.articles})
+            },
+            getNewsFr() {
+                this.axios
+                .get("https://newsapi.org/v2/top-headlines?country=fr&apiKey=2a9013172f7441c5b6f2e7a0d539660a")
+                .then((response) => {this.news = response.data.articles})
+            }
+        },
+        mounted() {
+            this.getNewsRu();
+        }
+    }
 </script>
+
+
+<style>
+    a {
+        box-sizing: border-box;
+        padding-left: 8px;
+    }
+    .new {
+        margin-top: 5px;
+    }
+    img {
+        margin-left: 16px;
+        height: 100px;
+        width: 200px;
+        background-size: contain;
+    }
+</style>
